5e9eaa5212e19118ef9dc229725650e14d5fe66b
Alice Campkin <alice.campkin@gmail.com>
2021-01-05 19:58:18 +0000

    installs Tailwind CSS and adds custom header

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="nav px-6 py-4">
+    <nav class="container mx-auto flex justify-between">
+      <h3 class="font-bold">Chat App</h3>
+      <div>
+        <a href="#" class="mx-4">Log In</a>
+        <a href="#" class="mx-4">Sign Up</a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
