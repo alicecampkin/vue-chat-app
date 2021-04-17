412fda4240615c5bde068532b03de32e0451c4f1
Alice Campkin <alice.campkin@gmail.com>
2021-04-17 20:50:12 +0100

    fix this mess

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { createStore } from 'vuex'
+import App from './App.vue'
+import './index.css'
 
+const app = createApp(App).mount('#app')
 // Create a new store instance.
 const store = createStore({
   state () {
@@ -15,7 +18,6 @@ const store = createStore({
   }
 })
 
-const app = createApp({ /* your root component */ })
 
 // Install the store instance as a plugin
 app.use(store)
